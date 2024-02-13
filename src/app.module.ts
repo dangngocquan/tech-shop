fdaa9cae59eab37d3dfb48130d15b337e542561c
dangngocquan <dangngocquan104@gmail.com>
2024-02-13 20:24:45 +0700

    feat: mails.module and auths.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from './user/entity/user.entity';
-import { UsersModule } from './user/users.module';
+import { User } from './users/entity/user.entity';
+import { UsersModule } from './users/users.module';
 
 
 @Module({
---
+++ src/auths/mails/mails.service.ts
@@ -0,0 +1,22 @@
+import { MailerService } from "@nestjs-modules/mailer";
+import { Injectable } from "@nestjs/common";
+
+@Injectable()
+
+export class MailsService {
+    constructor(
+        private mailerService: MailerService
+    ) {}
+
+    async sendEmailConfirmation(email: string, otp: string): Promise<any> {
+        return this.mailerService.sendMail({
+          to: email,
+          subject: '[Tech Shop] Email Verification',
+          template: './confirmation', // `.hbs` extension is appended automatically
+          context: {
+            name: email,
+            otp,
+          },
+        });
+    }
+}
